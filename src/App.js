57e09c1b863b135136bfb1fc4651a8540965623d
Ajila K Johnkutty <kjajila@gmail.com>
2023-02-24 15:29:39 -0500

    complete form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="generator">
+        <h2>Password Generator</h2>
+        <div className="generotor_password">
+          <button>ClipBoard</button>
+        </div>
+        <div className="form_group">
+          <label htmlFor="password-length">Password Length</label>
+          <input name="password-length" id="password-length" type="number" max="25" min="8"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="uppercase_letters">Include Uppercase Letters</label>
+          <input name="uppercase_letters" id="uppercase_letters" type="checkbox"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="lowercase_letters">Include Lowercase Letters</label>
+          <input name="lowercase_letters" id="lowercase_letters" type="checkbox"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="include_numbers">Include Numbers</label>
+          <input name="include_numbers" id="include_numbers" type="checkbox"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="include_symbols">Include Symbols</label>
+          <input name="include_symbols" id="include_symbols" type="checkbox"/>
+        </div>
+      </div>
     </div>
   );
 }
